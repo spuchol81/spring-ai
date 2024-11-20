4753e824ac6c77bea924009872abd1f4d4af7fdb
spuchol81 <spuchol@vmware.com>
2024-11-20 09:56:29 +0100

    doc

--- src/main/java/com/example/ChefService.java
+++ src/main/java/com/example/ChefService.java
@@ -3,7 +3,9 @@ package com.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
@@ -31,14 +33,15 @@ public class ChefService {
     }
 
 
+
     public Flux<String> answerCustomerQuestion(String question) {
         log.info("Answering question from Chat");
         var advisorSearchRequest = SearchRequest.query(question).withTopK(1).withSimilarityThreshold(0.8);
         var advise = new PromptTemplate(chefCookBook).getTemplate();
         return chatClient.prompt()
-                        .system(chef)
+                        //.system(chef)
                         .user(question)
-                        .advisors(new QuestionAnswerAdvisor(vectorStore, advisorSearchRequest, advise))
+                        //.advisors(new QuestionAnswerAdvisor(vectorStore, advisorSearchRequest, advise))
                         .stream().content();
     }
 
