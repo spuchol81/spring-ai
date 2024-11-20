0b285c9718e3d9faefa58a3b31f273ee8a869fe5
spuchol81 <spuchol@vmware.com>
2024-11-20 09:57:49 +0100

    Revert "doc"
    This reverts commit 4753e824ac6c77bea924009872abd1f4d4af7fdb.

--- src/main/java/com/example/RecipeFinderConfiguration.java
+++ src/main/java/com/example/RecipeFinderConfiguration.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class RecipeFinderConfiguration {
 
